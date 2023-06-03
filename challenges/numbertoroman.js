39305d28d9065f30a7ff556937e9f2253e800906
kalyani <kalyani.nelluri54@gmail.com>
2023-06-03 18:01:17 +0100

    test passed input number 1 output Roman I returned

--- challenges/numbertoroman.js
+++ challenges/numbertoroman.js
@@ -1,4 +1,15 @@
-export function numberToRoman(number)
+export function numberToRoman(num)
 {
-    return number;
-}
+    if (num !== 1) {
+        return ""; // Return an empty string for numbers other than 1
+      }
+    
+      return "I"; // Return "I" for number 1
+    }
+    const number = 1;
+const romanNumeral = numberToRoman(number);
+console.log(romanNumeral);
+
+    
+
+
