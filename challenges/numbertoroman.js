55f3b20d5027f902fc5cf9f1a14e18faebb04e5c
kalyani <kalyani.nelluri54@gmail.com>
2023-06-03 18:07:32 +0100

    implemented 1 to 10  with if condition test passed

--- challenges/numbertoroman.js
+++ challenges/numbertoroman.js
@@ -1,11 +1,29 @@
 export function numberToRoman(num)
 {
-    if (num !== 1) {
-        return ""; // Return an empty string for numbers other than 1
-      }
-    
-      return "I"; // Return "I" for number 1
+    if (num === 1) {
+        return "I";
+      } else if (num === 2) {
+        return "II";
+      } else if (num === 3) {
+        return "III";
+      } else if (num === 4) {
+        return "IV";
+      } else if (num === 5) {
+        return "V";
+      } else if (num === 6) {
+        return "VI";
+      } else if (num === 7) {
+        return "VII";
+      } else if (num === 8) {
+        return "VIII";
+      } else if (num === 9) {
+        return "IX";
+      } else if (num === 10) {
+        return "X";
+      } else {
+        return ""; // Return an empty string for numbers outside the range 1-10
     }
+}
     const number = 1;
 const romanNumeral = numberToRoman(number);
 console.log(romanNumeral);
