4b9fa0421ccb7024dba76faab7b27880e8669e21
kalyani <kalyani.nelluri54@gmail.com>
2023-06-03 18:31:08 +0100

    refactored using for and while loops giving any input number coverted to roman and return that roman number

--- challenges/numbertoroman.js
+++ challenges/numbertoroman.js
@@ -1,32 +1,17 @@
-export function numberToRoman(num)
-{
-    if (num === 1) {
-        return "I";
-      } else if (num === 2) {
-        return "II";
-      } else if (num === 3) {
-        return "III";
-      } else if (num === 4) {
-        return "IV";
-      } else if (num === 5) {
-        return "V";
-      } else if (num === 6) {
-        return "VI";
-      } else if (num === 7) {
-        return "VII";
-      } else if (num === 8) {
-        return "VIII";
-      } else if (num === 9) {
-        return "IX";
-      } else if (num === 10) {
-        return "X";
-      } else {
-        return ""; // Return an empty string for numbers outside the range 1-10
+export function numberToRoman(num, numeralToDigitMap) {
+    let result = '';
+  
+    for (const digit in numeralToDigitMap) {
+      while (num >= digit) {
+        result += numeralToDigitMap[digit];
+        num -= digit;
+      }
     }
-}
-    const number = 1;
-const romanNumeral = numberToRoman(number);
-console.log(romanNumeral);
+  
+    return result;
+  }
+  
+
 
     
 
--- test/numbertoroman.test.js
+++ test/numbertoroman.test.js
@@ -2,45 +2,41 @@ import {
     numberToRoman
   } from "../challenges/numbertoroman";
 
-  describe("numberToRoman", () => {
-    it("should convert number 1 to Roman numeral I", () => {
-      expect(numberToRoman(1)).toBe("I");
+  describe('numberToRoman', () => {
+    it('should convert number 1 to Roman numeral I', () => {
+      const numeralToDigitMap = {
+        1: 'I',
+      };
+      expect(numberToRoman(1, numeralToDigitMap)).toBe('I');
     });
   
-    it("should convert number 2 to Roman numeral II", () => {
-      expect(numberToRoman(2)).toBe("II");
+    it('should convert number 4 to Roman numeral IV', () => {
+      const numeralToDigitMap = {
+        4: 'IV',
+      };
+      expect(numberToRoman(4, numeralToDigitMap)).toBe('IV');
     });
   
-    it("should convert number 3 to Roman numeral III", () => {
-      expect(numberToRoman(3)).toBe("III");
+    it('should convert number 5 to Roman numeral V', () => {
+      const numeralToDigitMap = {
+        5: 'V',
+      };
+      expect(numberToRoman(5, numeralToDigitMap)).toBe('V');
     });
   
-    it("should convert number 4 to Roman numeral IV", () => {
-      expect(numberToRoman(4)).toBe("IV");
+    it('should convert number 9 to Roman numeral IX', () => {
+      const numeralToDigitMap = {
+        9: 'IX',
+      };
+      expect(numberToRoman(9, numeralToDigitMap)).toBe('IX');
     });
   
-    it("should convert number 5 to Roman numeral V", () => {
-      expect(numberToRoman(5)).toBe("V");
+    it('should convert number 10 to Roman numeral X', () => {
+      const numeralToDigitMap = {
+        10: 'X',
+      };
+      expect(numberToRoman(10, numeralToDigitMap)).toBe('X');
     });
+});
   
-    it("should convert number 6 to Roman numeral VI", () => {
-      expect(numberToRoman(6)).toBe("VI");
-    });
-  
-    it("should convert number 7 to Roman numeral VII", () => {
-      expect(numberToRoman(7)).toBe("VII");
-    });
-  
-    it("should convert number 8 to Roman numeral VIII", () => {
-      expect(numberToRoman(8)).toBe("VIII");
-    });
-  
-    it("should convert number 9 to Roman numeral IX", () => {
-      expect(numberToRoman(9)).toBe("IX");
-    });
-  
-    it("should convert number 10 to Roman numeral X", () => {
-      expect(numberToRoman(10)).toBe("X");
-    });
-  });
   
